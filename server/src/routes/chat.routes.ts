c74099d8b4b20748a93645a9c793ac85e6c695da
Daniel Obasuyi <danielobasuyi@gmail.com>
2025-11-24 13:54:10 +0100

    chore: remove server and duplicate client folder - clean frontend only

--- server/src/routes/chat.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { chatController } from '../controllers/chat.controller';
-
-const router = Router();
-
-router.post('/', chatController);
-
-export default router;
--- server/src/routes/code-helper.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { codeHelperController } from '../controllers/code-helper.controller';
-
-const router = Router();
-
-router.post('/', codeHelperController);
-
-export default router;
